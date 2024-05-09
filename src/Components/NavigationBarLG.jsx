c6dd89ec92fcac06f19cbc752e089f8c7596dfca
Angkit Khadka <khadkaankit85@gmail.com>
2024-05-08 22:54:49 -0400

    full screen navigation bar added

--- src/Components/NavigationBarLG.jsx
+++ src/Components/NavigationBarLG.jsx
@@ -18,40 +18,45 @@ const LargeUncollapsedNavigationBar = () => {
     //getting the theme from store:
     const theme = useSelector((state) => state.theme.value)
 
-
-
-
     return (
-        <nav className=" border-solid border-white border-2 fixed left-0 w-[270px] md:w-[300px] h-[100vh]">
+        <nav className=" border-white fixed left-0 w-[270px] md:w-[300px] h-[100vh]">
             <div>
                 <img src={theme === "darkTheme" ? darkInsta : lightInsta} alt="instagram logo" width={"60%"} height={"40px"} className="m-1 mx-0" />
             </div>
 
-            <section className="w-full h-[50vh] mt-1 flex flex-col items-center border-solid border-white border-2 ">
+            <section className="w-full h-[50vh] mt-1 flex flex-col items-center border-solid ">
 
-                <div className="flex w-full border p-6 gap-9 h-fit mb-4  items-center">
+                <div className="flex w-full p-6 gap-9 h-fit mb-4 rounded-xl items-center hover:bg-[#a3a1a1] hover:text-black" onClick={() => {
+                    window.location = "/home"
+                }}>
                     <img src={theme == "lightTheme" ? homeIconDark : homeIconLight} alt="home icon" width={"40px"} height={"40px"} className="  " />
-                    <h1 className="text-white">Home</h1>
+                    <h1 className={theme == "darkTheme" ? "text-white" : "text-black"}>Home</h1>
                 </div>
 
-                <div className="flex w-full border p-6 gap-9 mb-4 items-center">
+                <div className="flex w-full p-6 gap-9 mb-4 rounded-xl items-center hover:bg-[#a3a1a1]" onClick={() => {
+                    window.location = "/searchbar"
+                }}>
                     <img src={theme == "lightTheme" ? searchIconDark : searchIconLight} alt="home icon" width={"40px"} height={"40px"} className="  " />
-                    <h1 className="text-white">Search</h1>
+                    <h1 className={theme == "darkTheme" ? "text-white" : "text-black"}>Search</h1>
                 </div>
 
-                <div className="flex w-full border p-6 gap-9 mb-4 items-center">
+                <div className="flex w-full  p-6 gap-9 mb-4 rounded-xl items-center hover:bg-[#a3a1a1]" onClick={() => {
+                    window.location = "/messages"
+                }}>
                     <img src={theme == "lightTheme" ? messengerIconDark : messengerIconLight} alt="home icon" width={"40px"} height={"40px"} className="  " />
-                    <h1 className="text-white">Messages</h1>
+                    <h1 className={theme == "darkTheme" ? "text-white" : "text-black"}>Messages</h1>
                 </div>
-
-
-
-
-
-
 
             </section>
 
+            <section className="mt-[120px]">
+                <div className="flex w-full  p-6 gap-9 rounded-xl  mb-4 items-center hover:bg-[#a3a1a1]" onClick={() => {
+                    window.location = "/profile"
+                }}>
+                    <img src={theme == "lightTheme" ? userIconDark : userIconLight} alt="home icon" width={"40px"} height={"40px"} className="  " />
+                    <h1 className={theme == "darkTheme" ? "text-white" : "text-black"}>Profile</h1>
+                </div>
+            </section>
 
         </nav>
     )
