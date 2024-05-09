618ed541bea5fc6c0191c9e11f994bd4eb34701a
Angkit Khadka <khadkaankit85@gmail.com>
2024-05-08 21:55:59 -0400

    icons for theme added

--- src/Components/NavigationBarLG.jsx
+++ src/Components/NavigationBarLG.jsx
@@ -1,6 +1,12 @@
 import darkInsta from "../Assets/Images/instagram-image-black.svg"
+import homeIconDark from "../Assets/Images/ForApp/home_button.svg"
+import homeIconLight from "../Assets/Images/ForApp/LightStore/home_light.svg"
+import messengerIconDark from "../Assets/Images/ForApp/home_button.svg"
+import messengerIconLight from "../Assets/Images/ForApp/LightStore/messenger_icon_light.svg"
+import searchIconDark from "../Assets/Images/ForApp/Search_button.svg"
+import searchIconLight from "../Assets/Images/ForApp/LightStore/search_icon_light.svg"
+import userIconDark from "../Assets/Images/ForApp/User_icon.png"
 import { Link } from "react-router-dom"
-import Home from "../Pages/Home"
 const LargeUncollapsedNavigationBar = () => {
     return (
         <nav className=" border-solid border-white border-2 fixed left-0 w-[270px] md:w-[300px] h-[100vh]">
@@ -8,10 +14,9 @@ const LargeUncollapsedNavigationBar = () => {
                 <img src={darkInsta} alt="instagram logo" width={"60%"} height={"40px"} className="m-1 mx-0" />
             </div>
             <section className="w-full h-[40vh] mt-1  border-solid border-white border-2 ">
-                <div>
-                    <Link to={"/home"}>
-                        {/* <img src={} alt="" /> */}
-                    </Link>
+                <div className="flex w-[90%] border p-6 gap-9">
+                    <img src={homeIconLight} alt="home icon" width={"40px"} height={"40px"} className=" border " />
+                    <h1 className="text-white">Home</h1>
                 </div>
 
             </section>
