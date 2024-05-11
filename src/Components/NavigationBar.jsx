a453ffa5413ed949cc0979205c06f28039f991d7
Angkit Khadka <khadkaankit85@gmail.com>
2024-05-10 23:34:44 -0400

    navbar changes

--- src/Components/NavigationBar.jsx
+++ src/Components/NavigationBar.jsx
@@ -4,15 +4,15 @@ import SmallNavBar from "./NavigationBarSm"
 const NavigationBar = () => {
     return (
         <nav>
-            <div className="block md:hidden">
+            <div className="block md:hidden border border-white fixed left-0">
                 <SmallNavBar />
             </div>
 
-            <div className="hidden md:block lg:hidden sm:hidden">
+            <div className="hidden md:block lg:hidden sm:hidden border border-white">
                 <MediumNavBar />
             </div>
 
-            <div className=" hidden lg:block ">
+            <div className=" hidden lg:block border border-white fixed left-0 w-fit">
                 <LargeNavBar />
             </div>
 
