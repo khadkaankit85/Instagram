ac32c7e2e192b070af6802a042455a4930ecef87
Angkit Khadka <khadkaankit85@gmail.com>
2024-05-12 00:20:44 -0400

    outline for post initiated

--- src/Components/Inner Components/NewsFeed.jsx
+++ src/Components/Inner Components/NewsFeed.jsx
@@ -1,8 +1,10 @@
-
+import Post from "../Post"
 const NewsFeed = () => {
     return (
         <div>
             <h1 className="text-white">you have caught up to everything lol</h1>
+            <br />
+            <Post />
         </div>
     )
 }
---
+++ src/Components/Post.jsx
@@ -0,0 +1,9 @@
+const Post = () => {
+    return (
+        <div className="w-[485px] h-[660px] border border-red-500">
+            this is a post
+        </div>
+    )
+}
+
+export default Post
