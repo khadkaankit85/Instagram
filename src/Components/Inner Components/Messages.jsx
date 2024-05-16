2f44eec744610b40ba9540f10911713c0de592b7
Angkit Khadka <khadkaankit85@gmail.com>
2024-05-16 15:33:30 -0400

    Type :UI Completion Note: Message route completed with subroutes- '/' and 'chat'

--- src/Components/Inner Components/Messages.jsx
+++ src/Components/Inner Components/Messages.jsx
@@ -5,26 +5,91 @@ import GetChat from "../Subcomponents/GetChat"
 const Messages = () => {
 
     return (
-        <div className="flex border border-white justify-between items-center flex-row">
+        <div>
+            <Routes>
+                <Route path="/" element={
+                    <div className="w-full flex h-[calc(100vh-125px)] border border-white ">
+                        {/* this is chats section alright */}
+                        <div className="lg:w-[40%] md:w-[10%] overflow-auto w-full sm:w-[15%] border-r-2">
+                            <Chat />
+                            <Chat />
+                            <Chat />
+                            <Chat />
+                            <Chat />
+                            <Chat />
+                            <Chat />
+                            <Chat />
+                            <Chat />
+                            <Chat />
+                            <Chat />
+                            <Chat />
+                            <Chat />
+                            <Chat />
+                            <Chat />
+                        </div>
+                        <div className="justify-center items-center lg:w-[60%] sm:w-[90%] hidden sm:flex ">
+                            <NoMessages />
+                        </div>
 
-            <div className="md:w-[10%] lg:w-[40%] h-[80vh] border border-white w-[100%] overflow-auto" >
-                <Chat />
-                <Chat />
-                <Chat />
-                <Chat />
-                <Chat />
-            </div>
+                    </div>
+                } />
+                <Route path="/chat" element={
+                    <div>
+                        <div className="w-full flex h-[calc(100vh-125px)] border border-white ">
+                            {/* this is chats section alright */}
+                            <div className="lg:w-[40%] md:w-[10%] overflow-auto w-full sm:w-[15%] border-r-2 hidden md:block p-3">
+                                <Chat />
+                            </div>
+                            <div className="justify-center items-center lg:w-[60%] sm:w-[90%] p-3 ">
+                                <GetChat />
+                            </div>
 
-            <div className="w-[60%] lg:w-[60%]  md:w-[80%] h-[80vh] md:flex items-center justify-center sm:hidden hidden">
-                <Routes>
-                    <Route path="/" element={<NoMessages />} />
-                    <Route path="/chat" element={<GetChat />} />
-                </Routes>
-            </div>
+                        </div>
+                    </div>
+                } />
 
-
+            </Routes>
 
         </div>
+
+        // <div className="flex border border-white justify-between items-center ">
+
+        //     <div className="lg:w-[40%] w-[10%]">
+        //         <Routes>
+        //             <Route path="/" element={
+        //                 <div className="md:w-[90%] lg:w-[90%] h-[80vh] border border-white w-[100%] overflow-auto" >
+        //                     <Chat />
+        //                     <Chat />
+        //                     <Chat />
+        //                     <Chat />
+        //                     <Chat />
+        //                 </div>
+
+        //             } />
+        //             <Route path="/chat" element={
+        //                 <div className=" lg:w-[100%] h-[80vh] border border-white w-[100%] overflow-auto hidden sm:hidden md:block" >
+        //                     <Chat />
+        //                     <Chat />
+        //                     <Chat />
+        //                     <Chat />
+        //                     <Chat />
+        //                 </div>
+        //             } />
+        //         </Routes>
+        //     </div>
+
+
+        //     <div className=" md:flex items-center justify-center  lg:w-[60%]">
+        //         <Routes>
+
+        //             <Route path="/" element={<div className=" sm:hidden md:block hidden   "><NoMessages /></div>} />
+        //             <Route path="/chat" element={<div className="block w-full h-full"><GetChat /></div>} />
+        //         </Routes>
+        //     </div>
+
+
+
+        // </div>
     )
 }
 
--- src/Components/Subcomponents/GetChat.jsx
+++ src/Components/Subcomponents/GetChat.jsx
@@ -3,20 +3,25 @@ import TextMessageReceived from "./TextMessage"
 import TextMessageSent from "./TextMessageSent"
 const GetChat = () => {
     return (
-        <div className="flex flex-col border border-red-600 p-3 w-[100%] h-[100%]">
-            <div className="h-[80px]">
+        <div className="flex flex-col border border-red-600 p-3 h-[calc(100vh-130px)] w-full ">
+            <div className=" w-full">
                 <Link to={"/home/profile"} className="flex justify-between flex-row ">
-                    <div className="rounded-[50%] m-1 w-[%] lg:w-[10%] md:w-[100%] flex justify-center">
+                    <div className="rounded-[50%] m-1 w-[%] object-fcover lg:w-[10%] md:w-[100%] flex justify-center">
                         <img src="https://scontent-ord5-2.cdninstagram.com/v/t51.2885-19/427577770_351316991145367_2922071059885625484_n.jpg?stp=dst-jpg_s100x100&_nc_cat=102&ccb=1-7&_nc_sid=3fd06f&_nc_ohc=tmmdi3ITjnUQ7kNvgG_vq1S&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent-ord5-2.cdninstagram.com&oh=00_AYBEJZcHPqBmANT0F06BGsig82gc6RZ7cmFupmZEnh5xsg&oe=664AEF84" width={"60px"} height={"60px"} className="rounded-[50%]" alt="" />
                     </div>
-                    <div className="w-[100%] p-5 md:hidden lg:block ">
+                    <div className="w-[100%] p-5  ">
                         <h1 className="text-white">Ankit Khadka</h1>
                     </div>
 
                 </Link>
 
             </div>
-            <div className="h-[calc(100%-40px)] border p-2 border-blue-800 text-white overflow-auto">
+            <div className="h-[calc(100%-125px)] border p-2 border-blue-800 text-white overflow-auto">
+                <TextMessageSent />
+                <TextMessageSent />
+                <TextMessageSent />
+                <TextMessageSent />
+                <TextMessageSent />
                 <TextMessageSent />
                 <TextMessageSent />
                 <TextMessageSent />
